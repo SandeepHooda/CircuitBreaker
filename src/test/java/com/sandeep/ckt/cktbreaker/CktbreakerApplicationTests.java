fac3ad0c0647f0ee474a8cefc4cb474a2d90adfe
SandeepHooda <sonu.hooda@gmail.com>
2021-11-18 20:29:02 +0530

    first commit

---
+++ src/test/java/com/sandeep/ckt/cktbreaker/CktbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.ckt.cktbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CktbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
